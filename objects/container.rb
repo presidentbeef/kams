2245bf03f6716b5cf1fd41a1cd117f90ab4c397b
presidentbeef <justin@presidentbeef.com>
2011-03-10 03:25:52 +0000

    Tabs to spaces

--- objects/container.rb
+++ objects/container.rb
@@ -4,75 +4,75 @@ require 'traits/hasinventory.rb'
 #Container is used extensively inside the codebase, though essentially it is just a GameObject
 #with an Inventory that propogates events to its contents.
 class Container < GameObject
-	include HasInventory
+  include HasInventory
 
-	#Create new container. Capacity is unlimited if unset.
-	def initialize(capacity = nil, *args)
-		super(*args)
-		@inventory = Inventory.new(capacity)
-	end
+  #Create new container. Capacity is unlimited if unset.
+  def initialize(capacity = nil, *args)
+    super(*args)
+    @inventory = Inventory.new(capacity)
+  end
 
-	#Add an object to the container.
-	def add(object)
-		@inventory << object
-		object.container = @game_object_id
-	end
+  #Add an object to the container.
+  def add(object)
+    @inventory << object
+    object.container = @game_object_id
+  end
 
-	#Propagates alert to all in the container.
-	def alert(event)
-		@inventory.each { |o|
-			o.alert(event)
-		}
-	end
+  #Propagates alert to all in the container.
+  def alert(event)
+    @inventory.each { |o|
+      o.alert(event)
+    }
+  end
 
-	#Remove object from room.
-	def remove(object)
-		@inventory.remove(object)
-		object.container = nil
-	end
+  #Remove object from room.
+  def remove(object)
+    @inventory.remove(object)
+    object.container = nil
+  end
 
-	#Find a GameObject with that name in the container. Returns the GameObject if found, else nil. Actually just calls Inventory#find
-	def find(object_name, type = nil)
-		@inventory.find(object_name, type = nil)
-	end
+  #Find a GameObject with that name in the container. Returns the GameObject if found, else nil. Actually just calls Inventory#find
+  def find(object_name, type = nil)
+    @inventory.find(object_name, type = nil)
+  end
 
-	#Checks if the container contains the given id.
-	def include?(game_object_id)
-		@inventory.include? game_object_id
-	end
-	
-	#Outputs message to all in container, except those listed in skip.
-	def output(message, *skip)
-		skip = Set.new(skip)
+  #Checks if the container contains the given id.
+  def include?(game_object_id)
+    @inventory.include? game_object_id
+  end
+  
+  #Outputs message to all in container, except those listed in skip.
+  def output(message, *skip)
+    skip = Set.new(skip)
 
-		@inventory.each { |o|
-			o.output(message) unless skip.include?(o)
-		}
-	end
+    @inventory.each { |o|
+      o.output(message) unless skip.include?(o)
+    }
+  end
 
-	#Sends an event out to contents.
-	def out_event(event, *skip)
-		skip = Set.new(skip)
+  #Sends an event out to contents.
+  def out_event(event, *skip)
+    skip = Set.new(skip)
 
-		if event[:to_player] and event[:player] and @inventory.include? event[:player].goid
-			event[:player].out_event(event)
-			skip << event[:player]
-		end
+    if event[:to_player] and event[:player] and @inventory.include? event[:player].goid
+      event[:player].out_event(event)
+      skip << event[:player]
+    end
 
-		if event[:to_target] and event[:target] and @inventory.include? event[:target].goid
-			event[:target].out_event(event)
-			skip << event[:target]
-		end
+    if event[:to_target] and event[:target] and @inventory.include? event[:target].goid
+      event[:target].out_event(event)
+      skip << event[:target]
+    end
 
-		self.alert event if self.is_a? Reacts and not skip.include? self
+    self.alert event if self.is_a? Reacts and not skip.include? self
 
-		@inventory.each { |o|
-			o.out_event(event) unless skip.include?(o)
-		}
-	end
-	
-	#Returns a String describing contents.
-	def look_inside(event)
-		event[:player].output("#{self.name} contains:\n" << @inventory.show)
-	end
+    @inventory.each { |o|
+      o.out_event(event) unless skip.include?(o)
+    }
+  end
+  
+  #Returns a String describing contents.
+  def look_inside(event)
+    event[:player].output("#{self.name} contains:\n" << @inventory.show)
+  end
 end
